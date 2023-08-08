dae665f204f6033f7a9b415d94f9f44240f9e407
Anton Shpakovych <shpakovych18@gmail.com>
2023-08-08 18:47:50 +0300

    Added models Ticket, Order

--- cart/models.py
+++ cart/models.py
@@ -1,2 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+from airport.models import Flight
+
+
+USER = get_user_model()
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        USER,
+        related_name="orders",
+        on_delete=models.CASCADE
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        Order,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
